d4ee6722feb1ff208479c214b4479d81b5609f60
Mardaspan <imbert.kevin94@gmail.com>
2014-10-15 16:17:06 +0200

    ajout cartouche + modif afficherListe

--- TP5/listeCirculaire.cpp
+++ TP5/listeCirculaire.cpp
@@ -9,15 +9,23 @@ void initListeCirculaire(maillon **m){
     *m = NULL;
 }
 
+/*
+Procédure permettant l'affichage d'une liste circulaire
+Paramètre entrée: liste initialisée
+Paramètre sortie: affichage de la liste
+Pré-condition: la liste ne doit pas etre vide
+*/
 void afficherListe(maillon *p) {
         maillon *tmp;
 
-        tmp=p;
-        cout << "Maillon " << (*p).numero << endl;
-        tmp=(*tmp).suivant;
-        while (tmp != p) {
-            cout << "Maillon " << (*tmp).numero << endl;
+        if (p != null) {
+            tmp=p;
+            cout << "Maillon " << (*p).numero << endl;
             tmp=(*tmp).suivant;
+            while (tmp != p) {
+                cout << "Maillon " << (*tmp).numero << endl;
+                tmp=(*tmp).suivant;
+            }
         }
 }
 
