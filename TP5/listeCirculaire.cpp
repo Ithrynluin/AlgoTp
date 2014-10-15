e2179bc3b6c528a3481e6ef9bb69f818076deda6
Mardaspan <imbert.kevin94@gmail.com>
2014-10-15 15:58:43 +0200

    Ajout procedure afficheListe

--- TP5/listeCirculaire.cpp
+++ TP5/listeCirculaire.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
-
 using namespace std;
+
+void afficherListe(maillon *p) {
+        maillon *tmp;
+
+        tmp=p;
+        cout << "Maillon " << (*p).numero << endl;
+        tmp=(*tmp).suivant;
+        while (tmp != p) {
+            cout << "Maillon " << (*tmp).numero << endl;
+            tmp=(*tmp).suivant;
+        }
+}
