574f068f0184d2be28bd6f5073888019331f1519
clem19s <clemj1900@gmail.com>
2014-10-24 15:44:06 +0200

    sturct arbre

--- TP6/arbre.h
+++ TP6/arbre.h
@@ -1,6 +1,12 @@
 #ifndef ARBRE_H_INCLUDED
 #define ARBRE_H_INCLUDED
 
+typedef struct n {
+    char etiquette;
+    struct n* ag;
+    struct n* ad;
+} noed;
 
+noed * abin;
 
 #endif // ARBRE_H_INCLUDED
