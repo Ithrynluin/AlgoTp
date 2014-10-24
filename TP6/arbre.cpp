db8795ee042fbb7320d659b55033939d175b3eec
Mardaspan <imbert.kevin94@gmail.com>
2014-10-24 16:06:57 +0200

    MaJ

--- TP6/arbre.cpp
+++ TP6/arbre.cpp
@@ -2,10 +2,21 @@
 #include "arbre.h"
 using namespace std;
 
+/*
+Procédure qui initialise un arbre
+Paramètre de sortie: arbre initialisé
+Post-condition: le pointeur (abin) pointe sur rien
+*/
 void arbnouv (abin a) {
     a = NULL;
 }
-
+/*
+Procédure qui supprimer tous les élèment de l'arbre
+Paramètre d'entrée: arbre à détruire
+Paramètre de sortie: arbre détruit
+Pré-condition: L'arbre doit être initialisé
+Post-condition: L'arbre est détruit
+*/
 void arbdetruit (abin a) {
     if(a != NULL) {
         arbdetruit((*a).ag);
@@ -13,5 +24,29 @@ void arbdetruit (abin a) {
         delete (a);
     }
 }
-
-
+/*
+Fonction qui renvoie l'élément à gauche du noeud choisi
+Paramètre d'entrée : pointeur de noeud (abin)
+Paramètre de sortie : noeud à gauche de celui choisi dans l'arbre
+Pré-condition : L'arbre doit être initialisé
+*/
+noeud gauche (abin a) {
+    if (a != NULL) {
+        return (*a).ag;
+    } else {
+        return NULL;
+    }
+}
+/*
+Fonction qui renvoie l'élément à droite du noeud choisi
+Paramètre d'entrée : pointeur de noeud (abin)
+Paramètre de sortie : noeud à droite de celui choisi dans l'arbre
+Pré-condition : L'arbre doit être initialisé
+*/
+noeud droite (abin a) {
+    if(a != NULL) {
+        return (*a).ad;
+    } else {
+        return NULL;
+    }
+}
