1d9804773c7094b4f49f5e352ceda72a1c1095a4
Mardaspan <imbert.kevin94@gmail.com>
2014-10-24 15:53:21 +0200

    Ajout de linitialisation et desininit

--- TP6/arbre.cpp
+++ TP6/arbre.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
-
+#include "arbre.h"
 using namespace std;
+
+void arbnouv (abin a) {
+    a = NULL;
+}
+
+void arbdetruit (abin a) {
+    if(a != NULL) {
+        arbdetruit((*a).ag);
+        arbdetruit((*a).ad);
+        delete (a);
+    }
+}
+
+
